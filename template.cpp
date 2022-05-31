4d509742b9316b42c3f72f59487238c105cf0e6f
Gabriel Chiong <gabrielzhenchiong@gmail.com>
2022-05-31 14:43:18 +1000

    for running on windows

---
+++ template.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef pair<int, int> pi;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for (int i = a; i < b; ++i)
+
+int main() {
+  freopen("C:\\Users\\gabri\\Desktop\\cp\\template\\in.txt", "r", stdin);
+  freopen("C:\\Users\\gabri\\Desktop\\cp\\template\\out.txt", "w", stdout);
+
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  
+
+}
